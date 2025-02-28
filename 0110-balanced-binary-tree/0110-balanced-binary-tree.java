605229f273ee533e423d7de4d96bd6ea8610243a
RATHINAKUMAR SUNDARAJAN <117304767+RathinakumarSundarajan@users.noreply.github.com>
2025-02-28 14:24:26 +0530

    Time: 0 ms (100%), Space: 44.1 MB (96.93%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,10 +16,11 @@
 //  A Balanced Binary Tree is a binary tree where the height of the left and right subtrees of every node differ by at most one. 
 class Solution {
     public boolean isBalanced(TreeNode root) { 
+
         return getHeight(root) != -1;
     }
 
-    private int getHeight(TreeNode node) {
+       public int getHeight(TreeNode node) {
         if (node == null) {
             return 0; // Base case: height of an empty tree is 0
         }
@@ -27,17 +28,15 @@ class Solution {
         int leftHeight = getHeight(node.left);
         int rightHeight = getHeight(node.right);
 
-        // If any subtree is unbalanced, return -1 immediately
-        if (leftHeight == -1 || rightHeight == -1) {
+        if(leftHeight == -1 || rightHeight == -1){
             return -1;
         }
 
-        // If height difference is more than 1, return -1
-        if (Math.abs(leftHeight - rightHeight) > 1) {
+        if(Math.abs(leftHeight - rightHeight) > 1){
             return -1;
         }
 
-        // Otherwise, return height of current node
         return Math.max(leftHeight, rightHeight) + 1;
+        
     }
 }
